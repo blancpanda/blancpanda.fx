934e4b8c20bca16105657d3b542b1872a234955d
Kaoru Hojo <blancpanda@gmail.com>
2009-06-20 23:03:39 +0900

    プログレスバーをインスタンス化して使うように変更。ただし、更新時には表示した後消すことができない。よくわからない。

--- blancpanda/IndeterminateProgressBar.java
+++ blancpanda/IndeterminateProgressBar.java
@@ -5,6 +5,7 @@ package blancpanda;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -18,33 +19,55 @@ import javax.swing.border.EmptyBorder;
  * @author Kaoru
  * 
  */
-public class IndeterminateProgressBar {
+public class IndeterminateProgressBar extends JFrame {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private JLabel msg;
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		IndeterminateProgressBar.show("チャートを更新しています");
+		IndeterminateProgressBar progress = new IndeterminateProgressBar();
+		progress.showProgress("チャートを更新しています");
 	}
-	public static JFrame show(String str){
-		UIManager.put("ProgressBar.repaintInterval", new Integer(150));
-		UIManager.put("ProgressBar.cycleTime", new Integer(1050));
-		final JProgressBar aJProgressBar = new JProgressBar(0, 100);
+	
+	public IndeterminateProgressBar(){
+		super();
+		UIManager.put("ProgressBar.repaintInterval", new Integer(100));
+		UIManager.put("ProgressBar.cycleTime", new Integer(500));
+		JProgressBar aJProgressBar = new JProgressBar(0, 100);
 		aJProgressBar.setIndeterminate(true);
 		Border border = new EmptyBorder( 10, 10, 10, 10);
 		aJProgressBar.setBorder(border);
-		JLabel msg = new JLabel(str);
+		msg = new JLabel("");
 		border = new EmptyBorder( 10, 10, 10, 10);
 		msg.setBorder(border);
 		JPanel header = new JPanel();
 		header.add(msg, BorderLayout.CENTER);
-		JFrame theFrame = new JFrame();
-		theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Container contentPane = theFrame.getContentPane();
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container contentPane = getContentPane();
 		contentPane.add(aJProgressBar, BorderLayout.CENTER);
 		contentPane.add(header, BorderLayout.NORTH);
-		theFrame.setSize(300, 100);
-		theFrame.setVisible(true);
-		return theFrame;
+		setSize(300, 100);
+		setVisible(false);
+	}
+	
+	public void showProgress(final String text){
+		 //非同期処理
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+        		setVisible(true);
+        		msg.setText(text);
+            }
+        }); 
+	}
+	
+	public void hideProgress(){
+		setVisible(false);
 	}
 }
