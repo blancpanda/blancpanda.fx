8e6f2d25351523619cd67f306becaa13f1f24176
Kaoru Hojo <blancpanda@gmail.com>
2009-06-22 22:32:27 +0900

    取引時間チェックを追加。

--- blancpanda/fx/FXUtils.java
+++ blancpanda/fx/FXUtils.java
@@ -11,6 +11,7 @@ import java.net.URLConnection;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.TimeZone;
 
 import net.arnx.jsonic.JSON;
 import net.arnx.jsonic.JSONException;
@@ -35,6 +36,57 @@ public class FXUtils {
 	public static final long M30_SEGMENT_SIZE = M1_SEGMENT_SIZE * 30;
 	public static final long H1_SEGMENT_SIZE = M1_SEGMENT_SIZE * 60;
 	public static final long H2_SEGMENT_SIZE = H1_SEGMENT_SIZE * 2;
+	
+	/**
+	 * 取引時間チェック
+	 * @param date 日本時間のDate
+	 * @return true 取引時間内
+	 * @return false 取引時間外
+	 */
+	public static boolean isMarketTime(Date date){
+		boolean ret = false;
+
+		Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("Asia/Tokyo"));
+		cal.setTime(date);
+		
+		// America/New_York ニューヨーク時間でサマータイムかどうかチェック
+		TimeZone zone = TimeZone.getTimeZone("America/New_York");
+		boolean summertime = zone.inDaylightTime(cal.getTime());
+		int opentime = 7;
+		int closetime = 7;
+		if(summertime){
+			closetime = 6;
+		}
+		//System.out.println(cal.getTime());
+		if(cal.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY){
+			// 土曜日
+			if(cal.get(Calendar.HOUR_OF_DAY) >= closetime){
+				//System.out.println("取引時間外です。");
+				ret = false;
+			} else {
+				//System.out.println("取引時間内です。");
+				ret = true;
+			}
+		}else if(cal.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY){
+			// 日曜日
+			//System.out.println("取引時間外です。");
+			ret = false;
+		}else if(cal.get(Calendar.DAY_OF_WEEK) == Calendar.MONDAY){
+			// 月曜日
+			if(cal.get(Calendar.HOUR_OF_DAY) < opentime){
+				//System.out.println("取引時間外です。");
+				ret = false;
+			}else{
+				//System.out.println("取引時間内です。");
+				ret = true;
+			}
+		}else{
+			//System.out.println("取引時間内です。");
+			ret = false;
+		}
+		
+		return ret;
+	}
 	
 	@SuppressWarnings("unchecked")
 	public static HashMap getRateData(){
@@ -66,6 +118,14 @@ public class FXUtils {
 			System.out.println("JSONの解析に失敗しました");
 			return null;
 		}
+		
+		// JSONの解析に成功しても、timestampが取引時間外ならnullを返す
+		Calendar cal = Calendar.getInstance();
+		cal.setTimeInMillis(Long.parseLong((String) map.get("timestamp")));
+		if(!isMarketTime(cal.getTime())){
+			return null;
+		}
+		
 		return map;
 	}
 	
